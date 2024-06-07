4be6e833cf978e6ba876bfc95db43e65ed73fb16
William1234567891011121314 <williamboeira429@gmail.com>
2024-06-07 13:57:56 -0300

    inicio2

--- Usuario.hpp
+++ Usuario.hpp
@@ -1,1 +1,14 @@
-class 
+#include <iostream>
+#include <vector>
+class Usuario {
+public:
+	std::string login;
+	std::vector<Anuncio> favoritos;
+	std::vector<Anuncio> anuncios;
+	std::vector<Compra> compras;
+protected:
+	std::string email;
+	std::string telefone;
+	std::string endereco;
+	std::string	
+};
