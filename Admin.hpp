6f6527e9b7e1b7212b4f9f3f3dde6583c33dbfb0
William1234567891011121314 <williamboeira429@gmail.com>
2024-06-10 11:02:06 -0300

    Ponteiro

--- Admin.hpp
+++ Admin.hpp
@@ -4,7 +4,7 @@
 #include "Usuario.hpp"
 
 class Admin : public Usuario {
-	bool banirUsuario(Usuario* usuarioBanir, std::time_t* tempoDeBanimento);
+	bool banirUsuario(Usuario* usuarioBanir, std::time_t tempoDeBanimento);
 	bool unbanUsuario(Usuario* usuarioDesbanir);
 	bool excluirAnuncio(Anuncio* anuncioDeletar);
 };
