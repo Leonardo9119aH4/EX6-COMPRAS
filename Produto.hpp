76731764da2075f91400054278ab1fe59c2a5835
William1234567891011121314 <williamboeira429@gmail.com>
2024-06-14 09:53:12 -0300

    Sync 14/06 (1)

--- Produto.hpp
+++ Produto.hpp
@@ -4,7 +4,7 @@
 #define PRODUTO_HPP
 class Produto {
 public:
-	Produto();
+	Produto(std::string nome, std::string descricao, std::vector<std::string> tipos);
 	~Produto();
 	std::string nome, descricao;
 	std::vector<std::string> tipos;
