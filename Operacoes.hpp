8a85adbbd84da78928ebcd63ebd5f898346658da
Leonardo9119aH4 <leonardoartursasse@gmail.com>
2024-06-18 11:28:51 -0300

    sync 3?

---
+++ Operacoes.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "Anuncio.hpp"
+#include "Compra.hpp"
+#include "Produto.hpp"
+#include "Usuario.hpp"
+#include "Admin.hpp"
+#ifndef OPERACOES_HPP
+#define OPERACOES_HPP
+namespace Operacoes {
+	void OpcUsuario(std::vector<Usuario>* usuarios, Usuario* usuario, bool isAdmin, int* countId, std::vector<Anuncio*>* anuncios);
+	void NovoAnuncio(Usuario* usuario, int* countId, std::vector<Anuncio*>* anuncios);
+	void ViewAnuncio(std::vector<Anuncio*>* anuncios);
+	void VerCarrinho(Usuario* usuario);
+	void ComprarTudo(Usuario* usuario);
+	void pesquisar(std::vector<Anuncio*>* anuncios);
+}
+#endif // !OPERACOES_HPP
